3da7892dd67a9155c03354e7786e06c853747427
cristianvpe <tandemcristianvp@gmail.com>
2024-07-10 11:46:21 +0200

    añado perfil

---
+++ src/components/profile.js
@@ -0,0 +1,28 @@
+// src/components/Profile.js
+import React from "react";
+import '../components/layout.css';
+import { StaticImage } from "gatsby-plugin-image";
+
+const Profile = () => {
+  return (
+    <div className="content-container">
+      <h2 className="page-title">Perfil del Usuario</h2>
+      <div className="profile-container">
+      <StaticImage
+              src="../images/avatar.png"
+              alt="avatar"
+              className="avatar"
+              style={{ width: '100px', height: '100px' }}
+            />
+        <p className="profile-item">Nombre: {localStorage.getItem('tandem_nombre')}</p>
+        <p className="profile-item">Email: {localStorage.getItem('tandem_email')}</p>
+        <p className="profile-item">Id: {localStorage.getItem('tandem_id')}</p>
+        <a href="/contrasenia" role="button" className="btn btn-primary">
+          Cambiar contraseña
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,28 +1,28 @@
-import React from "react"
-import CerrarSesion from "../components/cerrarsesion"
-import Layout from "../components/layout"
-import Header from "../components/header"
-import Footer from "../components/footer"
-import VolverBoton from "../components/volverboton"
+// src/pages/ProfilePage.js
+import React from "react";
+import CerrarSesion from "../components/cerrarsesion";
+import Layout from "../components/layout";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import VolverBoton from "../components/volverboton";
+import Profile from "../components/profile";
+import '../components/layout.css';
 
 
-const Profile = () => {
+const ProfilePage = () => {
   return (
-    <>
-      <Layout>
-        <Header></Header>
-        <div className="qr-contenido">
-      <p>Nombre: {localStorage.getItem('tandem_nombre')}</p>
-      <p>Email: {localStorage.getItem('tandem_email')}</p>
-      <p>Id: {localStorage.getItem('tandem_id')}</p>
-      </div>
-      <a href="/contrasenia" role='button' className='aform'>Cambiar contraseña</a>
-      <br></br>
-      <CerrarSesion></CerrarSesion>
-      <VolverBoton></VolverBoton>
-      <Footer></Footer>
-      </Layout>
-    </>
-  )
-}
-export default Profile
+    <Layout>
+      <Header />
+      <CerrarSesion />
+      <main className="main-content">
+        <Profile />
+        <div className="actions">
+          <VolverBoton />
+        </div>
+      </main>
+      <Footer />
+    </Layout>
+  );
+};
+
+export default ProfilePage;
