739a0fd4742602ca250e31192a099d435464bce9
cristianvpe <tandemcristianvp@gmail.com>
2024-07-15 12:22:47 +0200

    actualizar pagina profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -17,7 +17,7 @@ const Profile = () => {
         <p className="profile-item">Nombre: {localStorage.getItem('tandem_nombre')}</p>
         <p className="profile-item">Email: {localStorage.getItem('tandem_email')}</p>
         <p className="profile-item">Id: {localStorage.getItem('tandem_id')}</p>
-        <a href="/contrasenia" role="button" className="btn btn-primary">
+        <a href="/" role="button" className="btn btn-primary">
           Cambiar contraseña
         </a>
       </div>
